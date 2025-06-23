3839854f0ee35a0c7cc13c01435cedbe766d7eee
Sahid <sahid2barroso@gmail.com>
2025-06-22 23:31:49 -0600

    nuevo correo

--- footer/footer/footer.php
+++ footer/footer/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <link rel="stylesheet" href="/PRODCONS/footer/footer/footer.css">
 <footer class="footer" style="padding-bottom:0; margin-bottom:0;">
     <div class="footer-container" style="padding-bottom:0; margin-bottom:0;">
@@ -23,7 +22,7 @@
 
         <!-- Columna 4: Correo -->
         <div class="footer3" style="flex:1; margin:0 15px 0 15px; min-width:100px; line-height:15px; margin-top:-50px; display:flex; flex-direction:column; align-items:flex-end; margin-right:40px; padding-right:15px;">
-            <p class="cfooter" style="font-size:15px; text-align:right; font-family:Georgia; margin-bottom:15px; font-weight:normal; color:#fbfbfb;">prodcons@gmail.com</p>
+            <p class="cfooter" style="font-size:15px; text-align:right; font-family:Georgia; margin-bottom:15px; font-weight:normal; color:#fbfbfb;">prodcons.blog@gmail.com</p>
             <p style="font-size:14px; color:#fbfbfb; margin-top:10px; text-align:right; font-family:Georgia; font-weight:normal;">Suscríbete Con Nosotros</p>
         </div>
     </div>
@@ -31,37 +30,3 @@
         © 2025 PRODCONS. Todos los derechos reservados.
     </div>
 </footer>
-=======
-<link rel="stylesheet" href="/PRODCONS/footer/footer/footer.css">
-<footer class="footer" style="padding-bottom:0; margin-bottom:0;">
-    <div class="footer-container" style="padding-bottom:0; margin-bottom:0;">
-        <!-- Columna 1: Logo -->
-        <img class="prodcons2" src='/PRODCONS/PI2do/imagenes/prodcon/prodcons_white.png' alt="Logo" style="width:220px; height:auto; margin-left:40px; box-shadow:none; border-radius:0;">
-        
-        <!-- Columna 2: Sobre Nosotros -->
-        <div class="footer-center" style="flex:2; max-width:500px; text-align:left; line-height:18px; padding-top:10px; margin:0 40px;">
-            <h4 style="font-size:20px; margin-bottom:10px; color:white; text-align:left;">SOBRE NOSOTROS</h4>
-            <p class="footer1" style="text-align:justify; font-size:16px; line-height:26px; font-family:Georgia; max-width:400px; color:white; margin:0; word-spacing: normal;">Somos una organización dedicada a cuidar del medio ambiente, aplicándolo en nuestra vida diaria y promoviendo a los demás a hacerlo para el bienestar de todos.</p>
-        </div>
-        
-        <!-- Columna 3: Enlaces legales -->
-        <div class="footer2">
-            <div class="footer-links-vertical">
-               <a href="/PRODCONS/PI2do/footer/parafooter/AvisoPriv/avisospriv.php">Avisos De Privacidad</a>
-               <a href="/PRODCONS/PI2do/footer/parafooter/politicas/politica.php">Política De Sostenibilidad</a>
-               <a href="/PRODCONS/PI2do/footer/parafooter/term-condi/term-condi.php">Términos Y Condiciones</a>
-            </div>
-        </div>
-
-
-        <!-- Columna 4: Correo -->
-        <div class="footer3" style="flex:1; margin:0 15px 0 15px; min-width:100px; line-height:15px; margin-top:-50px; display:flex; flex-direction:column; align-items:flex-end; margin-right:40px; padding-right:15px;">
-            <p class="cfooter" style="font-size:15px; text-align:right; font-family:Georgia; margin-bottom:15px; font-weight:normal; color:#fbfbfb;">prodcons@gmail.com</p>
-            <p style="font-size:14px; color:#fbfbfb; margin-top:10px; text-align:right; font-family:Georgia; font-weight:normal;">Suscríbete Con Nosotros</p>
-        </div>
-    </div>
-    <div style="text-align:right; color:white; font-family:Georgia; font-size:14px; padding:10px 40px 10px 0; margin-bottom:0;">
-        © 2025 PRODCONS. Todos los derechos reservados.
-    </div>
-</footer>
->>>>>>> main
